c27f95d3869dd76b75c40b9db23239f0d2954bb5
Alex Quach <alexhq1000@gmail.com>
2020-09-20 05:34:29 -0400

    Added seed generator to enable multi-player experience

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Link, BrowserRouter as Router, Redirect } from 'react-router-dom'
 
 import './App.css';
 import Picker from './Picker';
 
+function randomNum() {
+  return Math.floor(Math.random() * 1000000)
+}
+
 function App() {
+
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
@@ -15,6 +21,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <div> Color Picker! <br>
         </br>Generate a random color, and try your best to match it on the picker! Good luck!
         </div>
@@ -23,7 +30,16 @@ function App() {
         <div>Your Guess:</div>
         <div id="color-box"></div>
 
-        <Picker />
+        <Router>
+          <Route path="/colorpickergame/:id" component={Picker}></Route>
+          <Route exact path="/colorpickergame/"> 
+            <Redirect to={"/colorpickergame/" + randomNum()} />
+          </Route>
+        </Router>
+
+        <div style={{ marginTop: '20px' }}>
+            Share/Compete with friends by sending them the link!
+        </div>
 
       </header>
     </div>
--- src/Picker.js
+++ src/Picker.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
+import { useLocation } from "react-router-dom";
+import { useParams, Redirect, NavLink, Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { HexColorPicker } from "react-colorful";
 import "react-colorful/dist/index.css";
@@ -19,28 +21,38 @@ function color_meter(cwith, ccolor) {
   var __g = parseInt(_ccolor.substring(2, 4), 16);
   var __b = parseInt(_ccolor.substring(4, 6), 16);
 
-  var p1 = (_r / 255) * 100;
-  var p2 = (_g / 255) * 100;
-  var p3 = (_b / 255) * 100;
+  var p1 = Math.abs(((_r - __r) / 255) * 100);
+  var p2 = Math.abs(((_g - __g) / 255) * 100);
+  var p3 = Math.abs(((_b - __b) / 255) * 100);
 
-  var perc1 = Number.parseFloat((p1 + p2 + p3) / 3).toPrecision(5);
-
-  var p1 = (__r / 255) * 100;
-  var p2 = (__g / 255) * 100;
-  var p3 = (__b / 255) * 100;
-
-  var perc2 = Number.parseFloat((p1 + p2 + p3) / 3).toPrecision(5);
-
-  return Number.parseFloat(100 - Math.abs(perc1 - perc2)).toPrecision(5);
+  return Number.parseFloat(100 - (p1 + p2 + p3) / 3).toPrecision(5);
 }
+
+function randomNum() {
+  return Math.floor(Math.random() * 1000000)
+}
+
 function randomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
+function hashSeed(seed) {
+  return "#" + ((Math.floor(seed) * 6942069420) % 16777215).toString(16);
+}
+
 function Picker() {
+  // const location = useLocation();
+  // const seed = new URLSearchParams(location.search).get('session');
+  // console.log(seed)
+
+  const params = useParams();
+  const seed = params['id']
+  console.log(seed);
+
+
   const [color, setColor] = useState("#aabbcc");
   const [score, setScore] = useState("n/a");
-  const [true_color, setTrueColor] = useState(randomColor())
+  const [true_color, setTrueColor] = seed ? useState(hashSeed(seed)) : useState(randomColor());
 
   useEffect(() => {
     document.getElementById('color-box').style.backgroundColor = color
@@ -57,12 +69,20 @@ function Picker() {
         variant='outlined'
         color="inherit"
       >Score Me</Button>
+      
       <Button
         style={{ margin: '5px' }}
-        onClick={() => setTrueColor(randomColor())}
+        onClick={() => {
+          const num = randomNum();
+          <Redirect to={"/colorpickergame/" + num} />
+          setTrueColor(hashSeed(num));
+          }}
         variant='outlined'
         color="inherit"
-      >Randomize!</Button>
+      ><NavLink to={"/colorpickergame/" + randomNum()}
+          style={{color: "white", 'text-decoration': "none"}}
+      >Randomize!</NavLink></Button>
+      
 
       <span height="50px"></span>
       <div style={{ display: "flex", 'justify-content': "center" }}>
