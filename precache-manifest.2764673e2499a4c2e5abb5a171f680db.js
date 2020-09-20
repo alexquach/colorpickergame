1e295502989255995b751e39ff379bc3205f3b4c
Alex Quach <alexhq1000@gmail.com>
2020-09-19 22:25:55 -0400

    Updates

---
+++ precache-manifest.2764673e2499a4c2e5abb5a171f680db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d518562498334dc952031604c4395cf",
+    "url": "/colorpickergame/index.html"
+  },
+  {
+    "revision": "297de5ffe90dc489c6f4",
+    "url": "/colorpickergame/static/css/2.43074b11.chunk.css"
+  },
+  {
+    "revision": "d1641cd6af65030327f5",
+    "url": "/colorpickergame/static/css/main.636d2254.chunk.css"
+  },
+  {
+    "revision": "297de5ffe90dc489c6f4",
+    "url": "/colorpickergame/static/js/2.cc20a010.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/colorpickergame/static/js/2.cc20a010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1641cd6af65030327f5",
+    "url": "/colorpickergame/static/js/main.d68240ac.chunk.js"
+  },
+  {
+    "revision": "986179c771f26382dd69",
+    "url": "/colorpickergame/static/js/runtime-main.9eb48925.js"
+  }
+]);
